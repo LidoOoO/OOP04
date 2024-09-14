1b58d22ed37a0a9e813a5978666e1053f317c114
[200~LidoOoO~ <lidom899@gmail.com>
2024-09-14 17:50:38 +0300

    Part 03 Implment Interface (Implicitly Vs Explicitly)

---
+++ OOP04/Interfaces/IFlyable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces
+{
+    internal interface IFlyable
+    {
+        void Forward();
+
+        void Backward();
+
+        void Right();
+        void Left();
+
+    }
+}
