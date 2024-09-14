96d1da5e3d9cb7e9b39bedda26558e6486383f67
[200~LidoOoO~ <lidom899@gmail.com>
2024-09-14 16:50:26 +0300

    Part 02 Interface [Example 01]

---
+++ OOP04/Interfaces/ISeries.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces
+{
+    // Developer 01
+    internal interface ISeries
+    {
+        public int Current { get; set; }
+
+        public void GetNext();
+
+
+        public void Reset()
+        {
+            Current = 0;
+        }
+    }
+}
---
+++ OOP04/Interfaces/SeriesByFour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces
+{
+    internal class SeriesByFour : ISeries
+    {
+        public int Current { get; set; }
+
+        public void GetNext()
+           => Current += 4;
+
+
+    }
+}
