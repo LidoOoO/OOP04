5ff4e9fae193e2e0cc46799f5e14cb76bf37ca10
[200~LidoOoO~ <lidom899@gmail.com>
2024-09-16 19:52:52 +0300

    Part 07 Built-in Interfaces - ICloneable

---
+++ OOP04/Interfaces/ICloneable Interface/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP04.Interfaces.ICloneable_Interface
+{
+    internal class Employee : ICloneable 
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+
+
+        // Copy Constructor
+        public Employee(Employee employee)
+        {
+            Id = employee.Id;
+            Name = employee.Name;
+            Salary = employee.Salary;
+        }
+
+        public Employee()
+        {
+            
+        }
+
+        // DeepCopy  
+        public object Clone()
+        {
+            return new Employee(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id} , Name = {Name} , Salary = {Salary:c}";
+        }
+
+
+    }
+}
